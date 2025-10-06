14e1f6c5821f9c15e1805f1dc929c227c484d9ae
Magicalbat <magicalbat@comcast.net>
2025-10-05 20:04:48 -0400

    Update nvim conf to lazy.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,15 +1,2 @@
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
-require "user.colorscheme"
-require "user.nvim-tree"
+require "conf"
 
--- WHY
-require "nvim-tree".setup{}
-
-require "user.telescope"
-require "user.cmp"
-require "user.lsp"
-require "user.treesitter"
-require "user.autopairs"
---require "user.dap"
--- nvim/lua/conf/keymap.lua
+++ nvim/lua/conf/keymap.lua
@@ -71,33 +71,3 @@ keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 
-
-keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
--- keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
-keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
-
--- Nvimtree
-keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
-
-function create_header_guard()
-    -- Get the current file name
-    local file_name = vim.fn.expand("%:t")
-    if file_name == "" then
-        print("No file name found. Please save the file first.")
-        return
-    end
-
-    -- Generate the guard name (convert to uppercase and replace non-alphanumeric characters with '_')
-    local guard_name = file_name:upper():gsub("[^%w]", "_")
-
-    local header_guard = string.format("#ifndef %s\n#define %s\n\n\n\n#endif // %s", guard_name, guard_name, guard_name)
-
-    local cursor_pos = vim.api.nvim_win_get_cursor(0)
-    vim.api.nvim_buf_set_lines(0, cursor_pos[1] - 1, cursor_pos[1] - 1, false, vim.split(header_guard, "\n"))
-    vim.api.nvim_win_set_cursor(0, { cursor_pos[1] + 3, cursor_pos[2] })
-
-    print("Header guard added successfully.")
-end
-
-vim.api.nvim_set_keymap("n", "<Leader>hg", ":lua create_header_guard()<CR>", { noremap = true, silent = true })
-
